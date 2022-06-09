c13e76697bf8054d18ba55c4df3df71f0e28d2b7
Ubuntu <zur001@gmail.com>
2022-06-09 14:54:56 +0000

    renamed variables so the code will be easier to read

--- verifier.py
+++ verifier.py
@@ -3,15 +3,15 @@ import os
 import dot_finder
 
 
-def verify(half_one_arr: list, half_two_arr: list):
-    for i in half_one_arr:
+def verify(first_half_arr: list, second_half_arr: list):
+    for i in first_half_arr:
         dot = dot_finder.find(i[0])
-        for j in half_two_arr:
+        for j in second_half_arr:
             second_file_dot = dot_finder.find(j[0])
             if i[0][:dot] == j[0][:second_file_dot - 1] + "a":
-                fist_file_creation_time = i[1]
+                first_file_creation_time = i[1]
                 second_file_creation_time = j[1]
-                distance = fist_file_creation_time - second_file_creation_time
+                distance = first_file_creation_time - second_file_creation_time
                 minute_time = datetime.timedelta(0, 0, 0, 0, 1, 0, 0)
                 if distance <= minute_time:
                     return True
